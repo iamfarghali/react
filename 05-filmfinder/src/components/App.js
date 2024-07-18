a904d2c30b527e4aa952f911d3d0508224936a7f
Farghali <iamfarghali@gmail.com>
2024-07-18 17:26:20 +0300

    Wip

--- 05-filmfinder/src/components/App.js
+++ 05-filmfinder/src/components/App.js
@@ -1,7 +1,26 @@
+import { useReducer } from "react";
+
+const initialState = {
+  // loading, error, ready, finished
+  status: "ready",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "init": {
+      return "";
+    }
+
+    default:
+      throw new Error("This action doesnot exixt!");
+  }
+}
+
 function App() {
+  const [{ status }, dispatch] = useReducer(reducer, initialState);
   return <div></div>;
 }
 
 export default App;
 
-//https://www.omdbapi.com/
+// https://www.omdbapi.com/
